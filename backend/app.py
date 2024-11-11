9ac1dab74591c1192b927cabed1ca74fa3693ee4
muneeb10997 <muneeburrehman10997@gmail.com>
2024-11-11 14:06:47 +0500

    final revision backend task definition

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ app.config['MYSQL_HOST'] = os.getenv('DB_HOST')
 app.config['MYSQL_USER'] = os.getenv('DB_USER')  
 app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD') 
 app.config['MYSQL_DB'] = os.getenv('DB_NAME') 
-
+ 
 mysql = MySQL(app)
 
 # Health check route 
