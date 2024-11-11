223a0778dee02c15bc28ee33b1cb1fd45e5b4ffd
muneeb10997 <muneeburrehman10997@gmail.com>
2024-11-11 15:47:00 +0500

    check depolyment updated or not

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ mysql = MySQL(app)
 # Health check route 
 @app.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({'status': 'healthy'}), 200
+    return jsonify({'status': 'hello how are you this is depolyment testing'}), 200
 
 
 # Get all todos
