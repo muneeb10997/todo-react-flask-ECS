4c75fb209a1a07e02efbfd5346a6f6927ae4d189
muneeb10997 <muneeburrehman10997@gmail.com>
2024-11-11 13:52:32 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ CORS(app)
 app.config['MYSQL_HOST'] = os.getenv('DB_HOST')   
 app.config['MYSQL_USER'] = os.getenv('DB_USER')  
 app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD') 
-app.config['MYSQL_DB'] = os.getenv('DB_NAME')
+app.config['MYSQL_DB'] = os.getenv('DB_NAME') 
 
 mysql = MySQL(app)
 
